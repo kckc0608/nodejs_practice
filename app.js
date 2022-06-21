ab02458c1445cad116aea8b367b3baf3565265c4
kckc0608 <kckc0608@naver.com>
2022-06-21 17:38:36 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// module
+const express = require('express');
+const app = express();
+
+// routing
+const home = require('./routes/home');
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use('/', home);
+
+module.exports = app;
---
+++ bin/www.js
@@ -0,0 +1,7 @@
+'use strict';
+const app = require('../app');
+const port = 3030;
+
+app.listen(port, function(){
+	console.log('listening on ' + port);
+});
